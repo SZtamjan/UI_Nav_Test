e960ccbecd0711fd1e71303be147db3f80771d1a
SZtamjan <tamjan001@gmail.com>
2024-09-12 18:02:56 +0200

    base

---
+++ Assets/Scripts/SoulController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulController : MonoBehaviour
+{
+    private static SoulController instance;
+    public static SoulController Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = FindObjectOfType<SoulController>();
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+   
+
+
+    public List<SoulItem> Souls;
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Na scenie MainTest znajdziesz zadanie które trzeba wykonać.
+
+Checklista przy sprawdzaniu zadania:
+- Wszystko na scenie jest możliwe do obsłużenia bez używania myszki
+- Nawigacja po panelach i walce jest intuicyjna
+- Są widoczne podpowiedzi klawiszologii
+- Interfejsy nie otwierają się jeden na drugim, i możliwe jest sterowanie tylko po aktualnie otworzonym oknie/walce
